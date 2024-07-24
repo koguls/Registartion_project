5f1f9a2d2c3cc9bd42f5475f316906d9bb7b4c53
koguls <kogulkogul4@gmail.com>
2024-07-24 12:09:28 +0530

    code

--- registration.php
+++ registration.php
@@ -6,6 +6,25 @@
     <title>Registration From</title>
 </head>
 <body>
+    <div class="container">
+        <form action="registration.php" method="post">
+            <div class="form-group">
+                <input type="text" name="fullname" placeholder="Full Name:">
+            </div>
+            <div class="form-group">
+                <input type="email" name="email" placeholder="Email:">
+            </div>
+            <div class="form-group">
+                <input type="password" name="password" placeholder="Password:">
+            </div>
+            <div class="form-group">
+                <input type="text" name="repeat_password" placeholder="Repeat password">
+            </div>
+            <div class="form-group">
+                <input type="submit" value="Register" name="submit">
+            </div>
+        </form>
+    </div>
     
 </body>
 </html>
